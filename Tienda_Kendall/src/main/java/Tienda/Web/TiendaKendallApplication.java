5f341e9c3a9a10f877b0c42530ef64b5293600c6
Kgonzalez80699 <kgonzalez80699@ufide.ac.cr>
2025-05-21 19:14:31 -0600

    commit 1
    Mi primer commit

---
+++ Tienda_Kendall/src/main/java/Tienda/Web/TiendaKendallApplication.java
@@ -0,0 +1,13 @@
+package Tienda.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaKendallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaKendallApplication.class, args);
+	}
+
+}
