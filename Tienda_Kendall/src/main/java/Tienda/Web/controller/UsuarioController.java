863e095b9d7b3269e9afe86a83f73957a176bbbb
Kgonzalez80699 <kgonzalez80699@ufide.ac.cr>
2025-07-26 22:59:35 -0600

    Commit semana 11, comentada
    comentada

--- Tienda_Kendall/src/main/java/Tienda/Web/controller/UsuarioController.java
+++ Tienda_Kendall/src/main/java/Tienda/Web/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package Tienda.Web.controller;
 
+//todas las importaciones las cuales se van a usar
 import Tienda.Web.domain.Usuario;
 import Tienda.Web.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,43 +12,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+//Marca la clase como un controlador 
 @Controller
-@RequestMapping("/usuario")
+@RequestMapping("/usuario")//define que todas las clases van a comenzar por "/usuario"
 public class UsuarioController {
 
+    //Servicio que maneja la lógica relacionada con los usuarios
     @Autowired
     private UsuarioService usuarioService;
-
+    
+    //muestra el listado de todos los usuarios
     @GetMapping("/listado")
     public String listado(Model model) {
+        //obtiene la lista de los usuarios
         var usuarios = usuarioService.getUsuarios();
-        model.addAttribute("usuarios", usuarios);
-        model.addAttribute("totalUsuarios", usuarios.size());
-        return "/usuario/listado";
+        model.addAttribute("usuarios", usuarios);//agrega la lista de ususrios para que así la lista pueda acceder
+        model.addAttribute("totalUsuarios", usuarios.size());//agrega la cantidad total de usuarios
+        return "/usuario/listado";//devuelve la lista de /usuario/listado.html
     }
 
-    @GetMapping("/nuevo")
+    @GetMapping("/nuevo")//muestra le formulario para crear usuarios
     public String usuarioNuevo(Usuario usuario) {
-        return "/usuario/modifica";
+        return "/usuario/modifica";//devuleve la vista del formulario
     }
 
-    //@Autowired
-    //private FirebaseStorageService firebaseStorageService;
 
-    //@PostMapping("/guardar")
-    //public String usuarioGuardar(Usuario usuario,
-            //@RequestParam("imagenFile") MultipartFile imagenFile) {
-       // if (!imagenFile.isEmpty()) {
-            //usuarioService.save(usuario,false);
-           // usuario.setRutaImagen(
-       //             firebaseStorageService.cargaImagen(
-         //                   imagenFile,
-           //                 "usuario",
-             //               usuario.getIdUsuario()));
-        //}
-        //usuarioService.save(usuario,true);
-        //return "redirect:/usuario/listado";
-    //}
+
+    //guarda un usuario
+    @PostMapping("/guardar")
+    public String usuarioGuardar(Usuario usuario,
+            @RequestParam("imagenFile") MultipartFile imagenFile) {
+        if (!imagenFile.isEmpty()) {
+            usuarioService.save(usuario,false);
+            //usuario.setRutaImagen(
+        }
+        usuarioService.save(usuario,true);
+        return "redirect:/usuario/listado";
+    }
 
     @GetMapping("/eliminar/{idUsuario}")
     public String usuarioEliminar(Usuario usuario) {
--- Tienda_Kendall/src/main/java/Tienda/Web/dao/UsuarioDao.java
+++ Tienda_Kendall/src/main/java/Tienda/Web/dao/UsuarioDao.java
@@ -1,14 +1,19 @@
 package Tienda.Web.dao;
 
-import Tienda.Web.domain.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import Tienda.Web.domain.Usuario;//importa la clase Usuario de domain
+import org.springframework.data.jpa.repository.JpaRepository;//importa JpaRepository de spring 
+//La interfaz define métodos para que pueda acceder a la base de datos de la entidad Usuario.
+//A la hora de extender JpaRepository, hereda métodos como los son save(), delete(), findById(), findAll(), etc
 public interface UsuarioDao extends JpaRepository<Usuario, Long> {
+    //busca un usuario por el nombre, con esto se utiliza para poder iniciar sesion o verificar la existencia
     Usuario findByUsername(String username);
     
+    //busca un nombre o usuario y contraseña, con esto es util para poder hacer la verificacion
     Usuario findByUsernameAndPassword(String username, String Password);
-
+    
+    //puede usarse para evitar registros duplicados a la hora de validar el registro
     Usuario findByUsernameOrCorreo(String username, String correo);
-
+    
+    //con esto verifica si existe un usuario con el username o con el correo dado
     boolean existsByUsernameOrCorreo(String username, String correo);
 }
